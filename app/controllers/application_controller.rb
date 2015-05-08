36268bb8fc68f6c4d426f7666f635142fcad114e
Babar <knock@babar.im>
2015-05-09 05:39:37 +0600

    Restricted URLs only with HTML extentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :must_have_format
+
+  # Impose HTML format on all URLs
+  def default_url_options(options = nil)
+    {:format => "html"}
+  end
+
+  private
+    # Restrict URLs only with specific format
+    def must_have_format
+      raise ActionController::RoutingError.new('VulVal URL!') if params[:format].nil?
+    end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
+
+  skip_before_action :must_have_format
+
   def home
   end
 
