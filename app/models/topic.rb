172eb93ce450d19ccdb640fbfb625e5076a0c7a6
Babar <knock@babar.im>
2015-05-13 02:15:33 +0600

    Fix topic and question relations

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,8 @@
 class Topic < ActiveRecord::Base
   belongs_to :user
-  belongs_to :prerequisites_topic
+
+  has_many :child, class_name: "Topic", foreign_key: "prerequisites_topic_id"
+  belongs_to :prerequisites_topic, class_name: "Topic"
+
+  has_many :question
 end
