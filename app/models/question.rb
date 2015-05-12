9a6a39155ea9f025060e5455c88d0af5c4aa01db
Babar <knock@babar.im>
2015-05-13 03:18:05 +0600

    Initial Migration

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ActiveRecord::Base
   belongs_to :topic
 
   # Validation Rules
-  validates :title, presence: true, minimum: 20
+  validates :title, presence: true, length: {minimum: 20}
   validates :answer, presence: true, inclusion: { in: %w{a b c d} }
   validates :topic_id, presence: true
 end
