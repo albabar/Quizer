e9a6b2687b994bfd0c796228a5f49f4f142f2724
Babar <knock@babar.im>
2015-05-13 13:12:36 +0600

    Disable must have format filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :must_have_format
+  # before_action :must_have_format
 
   # Impose HTML format on all URLs
   def default_url_options(options = nil)
