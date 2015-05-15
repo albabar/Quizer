abf474dc0c3820d4361b484d7bd1c6c116fdbc3f
Babar <knock@babar.im>
2015-05-15 09:55:31 +0600

    Add questions insdie Topic single view

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,9 +2,10 @@ class Topic < ActiveRecord::Base
   belongs_to :user
 
   has_many :child, class_name: "Topic", foreign_key: "prerequisites_topic_id"
+  # has_many :prerequisite_topic, class_name: "Topic", foreign_key: "prerequisites_topic_id"
   belongs_to :prerequisites_topic, class_name: "Topic"
 
-  has_many :question
+  has_many :questions
 
   #Validation rules
   validates :title, presence: true
