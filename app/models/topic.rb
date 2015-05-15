040c0bfd9699bf5530605f18b4e3fb8a8e615879
Babar <knock@babar.im>
2015-05-15 10:04:28 +0600

    Allow Adding question forms inside topic form

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,11 +1,12 @@
 class Topic < ActiveRecord::Base
   belongs_to :user
 
+  #Setup Prerequisites Topic relation
   has_many :child, class_name: "Topic", foreign_key: "prerequisites_topic_id"
-  # has_many :prerequisite_topic, class_name: "Topic", foreign_key: "prerequisites_topic_id"
   belongs_to :prerequisites_topic, class_name: "Topic"
 
   has_many :questions
+  accepts_nested_attributes_for :questions, reject_if: :all_blank, allow_destroy: true
 
   #Validation rules
   validates :title, presence: true
